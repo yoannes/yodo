2c9a70ff91ed087ab9c450bea92ab5e2e1cde0f3
Yo <yoannes@gmail.com>
2024-10-12 11:39:58 +0900

    cp

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,6 @@
 export function logger(tag: string, ...msg: any[]) {
   //@ts-ignore
-  if (process.env.NODE_ENV !== "production" || window?.sageDebug) {
+  if (process.env.NODE_ENV !== "production" || window?.debug) {
     // biome-ignore lint/suspicious/noConsoleLog: <explanation>
     console.log(`[${tag}]: `, ...msg);
   }
