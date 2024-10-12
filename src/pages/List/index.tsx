d57b2f2ec324ab8e62d518566f49245d89939c14
yoannes@gmail.com <yoannes@gmail.com>
2024-10-12 22:04:20 +0900

    animation

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,8 +1,9 @@
 import { YodoButton, YodoDivider, YodoIcon, YodoInput } from "@components";
 import { bgBrandSubtle, textBrandEmphasis, textContent, textContentEmphasis } from "@consts";
+import autoAnimate, { getTransitionSizes } from "@formkit/auto-animate";
 import { useAuth, useI18n, useTasks } from "@hooks";
 import { cx } from "@utils";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { ListItem } from "./components";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
   const auth = useAuth();
   const tasks = useTasks();
   const [name, setName] = useState("");
+  const listRef = useRef<HTMLDivElement>(null);
 
   const list = useMemo(() => {
     return Object.values(tasks.state.list);
@@ -19,6 +21,61 @@ export default function Home() {
     if (!name) return;
     tasks.add(name);
   };
+
+  useEffect(() => {
+    if (listRef.current) {
+      autoAnimate(listRef.current, (el, action, oldCoords, newCoords) => {
+        let keyframes;
+        if (action === "add") {
+          keyframes = [
+            { transform: "scale(0)", opacity: 0 },
+            { transform: "scale(1.03)", opacity: 1, offset: 0.75 },
+            { transform: "scale(1)", opacity: 1 },
+          ];
+        }
+        if (action === "remove") {
+          keyframes = [
+            { transform: "scale(1)", opacity: 1 },
+            { transform: "scale(1.15)", opacity: 1, offset: 0.33 },
+            { transform: "scale(0.75)", opacity: 0.1, offset: 0.5 },
+            { transform: "scale(0.5)", opacity: 0 },
+          ];
+        }
+        if (action === "remain") {
+          const deltaX = (oldCoords?.left || 0) - (newCoords?.left || 0);
+          const deltaY = (oldCoords?.top || 0) - (newCoords?.top || 0);
+          const [widthFrom, widthTo, heightFrom, heightTo] = getTransitionSizes(
+            el,
+            oldCoords!,
+            newCoords!,
+          );
+          const start: Record<string, string> = {
+            transform: `translate(${deltaX}px, ${deltaY}px)`,
+          };
+          const mid: Record<string, string | number> = {
+            transform: `translate(${deltaX * -0.15}px, ${deltaY * -0.15}px)`,
+            offset: 0.75,
+          };
+          const end: Record<string, string> = { transform: `translate(0, 0)` };
+          if (widthFrom !== widthTo) {
+            start.width = `${widthFrom}px`;
+            mid.width = `${widthFrom >= widthTo ? widthTo / 1.05 : widthTo * 1.05}px`;
+            end.width = `${widthTo}px`;
+          }
+          if (heightFrom !== heightTo) {
+            start.height = `${heightFrom}px`;
+            mid.height = `${heightFrom >= heightTo ? heightTo / 1.05 : heightTo * 1.05}px`;
+            end.height = `${heightTo}px`;
+          }
+          keyframes = [start, mid, end];
+        }
+        return new KeyframeEffect(el, keyframes as Keyframe[], {
+          duration: 600,
+          easing: "ease-out",
+        });
+      });
+    }
+  }, [listRef]);
 
   return (
     <div className={root}>
@@ -54,7 +111,7 @@ export default function Home() {
       <div>
         <YodoDivider />
 
-        <div className="flex flex-col gap-3">
+        <div ref={listRef} className="flex flex-col gap-3">
           {list.map((item) => (
             <ListItem key={item.id} task={item} />
           ))}
