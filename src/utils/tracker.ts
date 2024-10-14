54598e0eed273239f6e6417bea0205d1f9e660df
yoannes@gmail.com <yoannes@gmail.com>
2024-10-14 23:55:20 +0900

    only dev

--- src/utils/tracker.ts
+++ src/utils/tracker.ts
@@ -1,18 +1,20 @@
 import mixpanel from "mixpanel-browser";
 
 if (!import.meta.env.DEV) {
+  mixpanel.init(import.meta.env.VITE_MIXPANEL, {
+    debug: import.meta.env.DEV,
+    track_pageview: true,
+    persistence: "localStorage",
+  });
 }
-mixpanel.init(import.meta.env.VITE_MIXPANEL, {
-  debug: import.meta.env.DEV,
-  track_pageview: true,
-  persistence: "localStorage",
-});
 
 export function identifyUser(userId: string) {
+  if (import.meta.env.DEV) return;
   mixpanel.identify(userId);
 }
 
 export function setUser(name: string, email: string) {
+  if (import.meta.env.DEV) return;
   mixpanel.people.set({
     $name: name,
     $email: email,
@@ -21,5 +23,6 @@ export function setUser(name: string, email: string) {
 }
 
 export function track(eventName: string, data: Record<string, any>) {
+  if (import.meta.env.DEV) return;
   mixpanel.track(eventName, data);
 }
